42cb30f2dd79764c2dd78113ea14c4d3d76740ca
Al Hussain <20220002117@students.cud.ac.ae>
2026-05-01 16:15:06 +0400

    Fix build: remove BorrowContext from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import BorrowProvider from "./context/BorrowContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BorrowProvider>
-    <App />
-  </BorrowProvider>
+  <App />
 );
