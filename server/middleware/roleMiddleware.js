771452868a4cf78c94c8927d155968a3e58ddb9d
Al Hussain <20220002117@students.cud.ac.ae>
2026-05-02 21:34:50 +0400

    Improve role middleware: add safety check for req.user

--- server/middleware/roleMiddleware.js
+++ server/middleware/roleMiddleware.js
@@ -1,8 +1,9 @@
 module.exports = (role) => {
   return (req, res, next) => {
-    if (req.user.role !== role) {
+    if (!req.user || req.user.role !== role) {
       return res.status(403).json({ message: "Access denied" });
     }
+
     next();
   };
 };
